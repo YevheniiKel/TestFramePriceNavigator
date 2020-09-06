5d61b81e79f94ac3ce8c582115fd9f0460c39293
lTsMaineCoon <asuskel97@gmail.com>
2020-09-06 15:08:48 +0300

    NullPointerException

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,15 +1,11 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Tests {
     public static WebDriver driver;
-
-    public static WebElement loginButton;
-    public static     WebElement emailField;
-    public static     WebElement passwordField;
-
 
     public static void setUP(){
         driver = new ChromeDriver();
@@ -17,21 +13,13 @@ public class Tests {
 
     public static void test() throws InterruptedException {
         driver.get("https://pn.com.ua/");
-
         System.out.println("driver=" + driver);
-//        MainPage page = new MainPage(driver);
-        loginButton = driver.findElement(By.xpath("//span[@class='user-info']"));
-
-        loginButton.click();
-        Thread.sleep(3000);
-        emailField = driver.findElement(By.xpath("//input[@id='login-form-login']"));
-        passwordField = driver.findElement(By.xpath("//input[@id='login-form-password']"));
-        emailField.sendKeys("dkjasdjk@CSJankd.ds");
-
+        MainPage page = new MainPage(driver);
+        System.out.println("driver=" + driver);
         Thread.sleep(1000);
-        passwordField.sendKeys("password");
+        // page.openLoginPopup().enterLogin().enterPass();
 
-        Thread.sleep(1000);
+
 
     }
     public static void tearDown(){
