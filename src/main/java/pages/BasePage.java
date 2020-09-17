f4f9ca48e4f82d5313bbe7ac49bad2a2e66d6153
lTsMaineCoon <asuskel97@gmail.com>
2020-09-17 15:28:21 +0300

    [YK] Changed BasePage into abstract class.

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import util.elementUtils.WaitUtils;
 
-public class BasePage {
+public abstract class BasePage {
     protected WebDriver driver;
     protected WaitUtils wait;
 
